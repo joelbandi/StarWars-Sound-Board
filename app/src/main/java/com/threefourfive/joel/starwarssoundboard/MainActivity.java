d7be89251e34d3d34126c432d0feb94ab957de55
joelbandi <joelvivekbandi@gmail.com>
2015-12-29 18:33:36 -0800

    second commit: Added sounds and tested for one button

--- app/src/main/java/com/threefourfive/joel/starwarssoundboard/MainActivity.java
+++ app/src/main/java/com/threefourfive/joel/starwarssoundboard/MainActivity.java
@@ -1,9 +1,18 @@
 package com.threefourfive.joel.starwarssoundboard;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+    MediaPlayer mplayer;
+    public void onClick1(View view){
+        if(view.getTag()=="0"){
+            mplayer = MediaPlayer.create(this,R.raw.chewbacca_01);
+            mplayer.start();
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
